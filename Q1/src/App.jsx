ea1bfacc85e500547e625af97af7f68d60c63c9a
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-03 20:20:17 +0530

    Mini Restaurant Management App

--- Q1/src/App.jsx
+++ Q1/src/App.jsx
@@ -3,15 +3,22 @@ import { AuthProvider } from "./AuthContext";
 import Customer from "./pages/Customer";
 import Admin from "./pages/Admin";
 import Login from "./pages/Login";
+import ProtectedRoute from './ProtectedRoute';
 
 function App(){
   return(
     <AuthProvider>
       <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/admin/dashboard" element={<Admin/>}/>
-        <Route path="/customers/dashboard" element={<Customer/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/admin/dashboard" element={
+          <ProtectedRoute role="admin">
+            <Admin/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/customers/dashboard" element={<ProtectedRoute role="customer">
+            <Customer/>
+          </ProtectedRoute>}/>
       </Routes>
       </BrowserRouter>
     </AuthProvider>
