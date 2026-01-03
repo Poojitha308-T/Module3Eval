5d83c99902c7588a97c4626117471a647ca73dc7
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-03 19:15:54 +0530

    Mini Restaurant Management App

---
+++ Q1/src/AuthContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export function AuthProvider({children}){
+    const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem("auth")===true);
+
+    const login = (email, password) =>{
+        if(email==="admin@gmail.com" && password === "admin1234"){
+            setIsAuthenticated(true);
+            localStorage.setItem("auth",true);
+            return true;
+        }
+        return false;
+    };
+    login = (email,password)=>{
+        if(email === "customer@gmail.com" && password==="customer1234"){
+            setIsAuthenticated(true);
+            localStorage.setItem("auth",true);
+            return true;
+        }
+        return false;
+
+    };
+
+    const logout=()=>{
+        setIsAuthenticated(false);
+        localStorage.removeItem("auth");
+    };
+
+    return(
+        <AuthContext.Provider value={{isAuthenticated,login,logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
---
+++ Q1/src/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "./AuthContext";
+import Navigate from "react-router-dom";
+
+export default function ProtectedRoute({children}){
+    const { isAuthenticated } = useContext(AuthContext);
+    return(
+        isAuthenticated? children:<Navigate to="/login" replace/>
+    )
+}
