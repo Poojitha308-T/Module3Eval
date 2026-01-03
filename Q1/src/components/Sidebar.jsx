165e681d5821fe41d4d9405576d1c11429a2846b
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-03 19:58:59 +0530

    Mini Restaurant Management App

---
+++ Q1/src/components/Sidebar.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function Sidebar(){
+    const [form, setForm] = useState({
+        restaurantId:"",
+        restaurantName:"",
+        address:"",
+        type:"",
+        parkingLot:"true",
+        image:""
+    })
+
+    return(
+        <div>
+            <img src="https://coding-platform.s3.amazonaws.com/dev/lms/tickets/7524df6e-46fa-4506-8766-eca8da47c2f1/2izhqnTaNLdenHYF.jpeg"/>
+            <input value={form.restaurantName} onChange={e=>setForm({...form, restaurantName: e.target.value})}/>
+            <input value={form.address} onChange={e=>setForm({...form, address: e.target.value})}/>
+            <select value={form.type} onChange={e=>setForm({...form, type:e.target.value })}>
+                <option value="">Select</option>
+                <option>Rajasthani</option>
+                <option>Gujarati</option>
+                <option>Mughlai</option>
+                <option>Jain</option>
+                <option>Thai</option>
+                <option>North Indian</option>
+                <option>South Indian</option>
+            </select>
+            <select value={form.parkingLot} onChange={e=>setForm({...form, parkingLot:e.target.value})}>
+                <option>True</option>
+                <option>False</option>
+            </select>
+            
+        </div>
+    )
+}
