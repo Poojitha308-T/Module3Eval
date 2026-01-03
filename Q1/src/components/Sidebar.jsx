2ffba36106133fb8a35118306f0bd69935818ab8
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-03 20:54:50 +0530

    Mini Restaurant Management App

--- Q1/src/components/Sidebar.jsx
+++ Q1/src/components/Sidebar.jsx
@@ -27,18 +27,13 @@ export default function Sidebar({
         <div>
             <img src="https://coding-platform.s3.amazonaws.com/dev/lms/tickets/7524df6e-46fa-4506-8766-eca8da47c2f1/2izhqnTaNLdenHYF.jpeg" width={"100px"}/>
             <p>Address</p>
-            <input value={form.restaurantName} onChange={e=>setForm({...form, restaurantName: e.target.value})}/>
-            <input value={form.address} onChange={e=>setForm({...form, address: e.target.value})}/>
-            <select value={form.type} onChange={e=>setForm({...form, type:e.target.value })}>
-                <option value="">Select</option>
-                <option>Rajasthani</option>
-                <option>Gujarati</option>
-                <option>Mughlai</option>
-                <option>Jain</option>
-                <option>Thai</option>
-                <option>North Indian</option>
-                <option>South Indian</option>
-            </select>
+            {auth.role==="admin"?(<select value={form.type} onChange={e=>setForm({...form, type:e.target.value })}>
+                {TYPES.map((t)=>(
+                    <option key={{t} value={t}}/>
+                ))}
+                )}
+            
+               
             <select value={form.parkingLot} onChange={e=>setForm({...form, parkingLot:e.target.value})}>
                 <option>True</option>
                 <option>False</option>
