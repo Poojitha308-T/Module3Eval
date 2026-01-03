c258b0ccdf20169c2f54bd892302c594db2bf031
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-01-03 20:53:03 +0530

    Mini Restaurant Management App

--- Q1/src/components/Sidebar.jsx
+++ Q1/src/components/Sidebar.jsx
@@ -1,26 +1,32 @@
+import { useContext } from "react";
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
-export default function Sidebar(){
-    const [form, setForm] = useState({
-        restaurantId:"",
-        restaurantName:"",
-        address:"",
-        type:"",
-        parkingLot:"true",
-        image:""
-    })
-    const handleSubmit=()=>
-    {
-        if(!form.restaurantName || !form.address || !form.parkingLot){
-            alert("All fields required")
-            return;
-        }
-        setForm({restaurantName:"", address:"", type:"", parkingLot:"true"});
-    };
+const TYPES=[
+    "Rajasthani",
+    "Gujarati","Mughali","Jain","Thai","North Indian","SouthIndian"
+];
+export default function Sidebar({
+    
+        restaurantId,
+        restaurantName,
+        address,
+        type,
+        parkingLot,
+        image,
+        setData
+    }){
+        const {auth}=useContext(AuthContext);
+        const navigate = useNavigate();
+        const[selectedType, setSelectedType] = useState(type);
+
+    }
+    
 
     return(
         <div>
             <img src="https://coding-platform.s3.amazonaws.com/dev/lms/tickets/7524df6e-46fa-4506-8766-eca8da47c2f1/2izhqnTaNLdenHYF.jpeg" width={"100px"}/>
+            <p>Address</p>
             <input value={form.restaurantName} onChange={e=>setForm({...form, restaurantName: e.target.value})}/>
             <input value={form.address} onChange={e=>setForm({...form, address: e.target.value})}/>
             <select value={form.type} onChange={e=>setForm({...form, type:e.target.value })}>
